8656d3a5b01f4548d5b7867280f5b5bdc0cebf11
kvin1087 <kaustubh87@gmail.com>
2017-06-04 11:19:58 -0500

    Added new route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+    
+Product.find(function(error, docs){
+    
+    res.render('shop/index', { title: 'Shopping Cart' , products: docs });
+    console.log(docs);
+});
+  
 });
 
 module.exports = router;
