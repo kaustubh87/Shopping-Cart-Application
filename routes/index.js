b514f6ba00a04da01332d979010671a4ccabaffa
kvin1087 <kaustubh87@gmail.com>
2017-06-06 11:14:54 -0500

    Added signup route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
 var csrf = require('csurf');
+var passport = require('passport');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
@@ -25,11 +26,19 @@ Product.find(function(error, docs){
 /* Get signup page*/
 
 router.get('/user/signup', function(req,res,next){
-   res.render('user/signup', { csrfToken: req.csrfToken()}); 
+   var messages = req.flash('error');
+   res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length> 0}); 
 });
 
+
+router.post('/user/signup', passport.authenticate('local.signup', {
+   successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
+    failureFlash: true
+}));
+            
+router.get('/user/profile', function(req,res,next){
+    res.render('user/profile');
+});
+            
 module.exports = router;
-
-router.post('/user/signup', function(req,res,next){
-   res.redirect('/'); 
-});
