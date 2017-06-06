fdbd7efa42d55e79e8a16c39014041701ce1e345
kvin1087 <kaustubh87@gmail.com>
2017-06-05 21:45:06 -0500

    Added signup get and post routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     
 Product.find(function(error, docs){
-    
-    res.render('shop/index', { title: 'Shopping Cart' , products: docs });
-    console.log(docs);
+    var productChunks = [];
+    var chunkSize = 3;
+    for(var i =0 ;i <docs.length; i += chunkSize){
+        productChunks.push(docs.slice(i, i+ chunkSize));
+        //console.log(productChunks);
+    }
+    res.render('shop/index', { title: 'Shopping Cart' , products: productChunks });
 });
   
 });
 
+
+/* Get signup page*/
+
+router.get('/user/signup', function(req,res,next){
+   res.render('user/signup', { csrfToken: req.csrfToken()}); 
+});
+
 module.exports = router;
+
+router.post('/user/signup', function(req,res,next){
+   res.redirect('/'); 
+});
