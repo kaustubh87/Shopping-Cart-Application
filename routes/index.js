3429531a5a33cacb2455879bff6e779261b84d9c
kvin1087 <kaustubh87@gmail.com>
2017-06-07 11:51:06 -0500

    Added index route

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,11 @@ Product.find(function(error, docs){
 
 router.get('/user/signup', function(req,res,next){
    var messages = req.flash('error');
-   res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length> 0}); 
+   res.render('user/signup', { 
+       csrfToken: req.csrfToken(),
+       messages: messages,
+       hasErrors: messages.length> 0
+   }); 
 });
 
 
@@ -36,9 +40,26 @@ router.post('/user/signup', passport.authenticate('local.signup', {
     failureRedirect: '/user/signup',
     failureFlash: true
 }));
+
             
 router.get('/user/profile', function(req,res,next){
     res.render('user/profile');
 });
+
+
+router.get('/user/signin', function(req,res,next){
+    var messages = req.flash('error');
+   res.render('user/signin', { 
+       csrfToken: req.csrfToken(), 
+       messages: messages, 
+       hasErrors: messages.length> 0
+   }); 
+});
+
+router.post('/user/signin', passport.authenticate('local.signin',{
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signin',
+    failureFlash: true
+}));
             
 module.exports = router;
