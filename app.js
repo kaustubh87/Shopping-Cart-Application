c0af9ba0e8643eaed85ae6d08055e86bf78bbd78
kvin1087 <kaustubh87@gmail.com>
2017-06-07 09:48:23 -0500

    Added express validator

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
 
 
 mongoose.connect('localhost:27017/shopping-cart', function(err){
@@ -16,6 +20,8 @@ mongoose.connect('localhost:27017/shopping-cart', function(err){
     console.log('Established connection to the database');
     
 });
+
+require('./config/passport');
 
 var index = require('./routes/index');
 
@@ -31,7 +37,13 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+//resave and saveUnitialized for storing session on the server for every request which we don't want so it has been set to false
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
