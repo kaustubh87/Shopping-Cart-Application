2e181442d80915752f286cfce390e24293a232e1
kvin1087 <kaustubh87@gmail.com>
2017-06-07 22:02:10 -0500

    moved user routes from this file

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
-var csrf = require('csurf');
-var passport = require('passport');
 
-var csrfProtection = csrf();
-router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,43 +19,5 @@ Product.find(function(error, docs){
 });
 
 
-/* Get signup page*/
-
-router.get('/user/signup', function(req,res,next){
-   var messages = req.flash('error');
-   res.render('user/signup', { 
-       csrfToken: req.csrfToken(),
-       messages: messages,
-       hasErrors: messages.length> 0
-   }); 
-});
-
-
-router.post('/user/signup', passport.authenticate('local.signup', {
-   successRedirect: '/user/profile',
-    failureRedirect: '/user/signup',
-    failureFlash: true
-}));
-
-            
-router.get('/user/profile', function(req,res,next){
-    res.render('user/profile');
-});
-
-
-router.get('/user/signin', function(req,res,next){
-    var messages = req.flash('error');
-   res.render('user/signin', { 
-       csrfToken: req.csrfToken(), 
-       messages: messages, 
-       hasErrors: messages.length> 0
-   }); 
-});
-
-router.post('/user/signin', passport.authenticate('local.signin',{
-    successRedirect: '/user/profile',
-    failureRedirect: '/user/signin',
-    failureFlash: true
-}));
             
 module.exports = router;
