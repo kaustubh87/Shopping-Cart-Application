b454776a1faad01ac7cfd90518bf63237c16f177
kvin1087 <kaustubh87@gmail.com>
2017-06-09 20:49:45 -0500

    Code for calculating items in the cart added

--- models/cart.js
+++ models/cart.js
@@ -1,13 +1,28 @@
-module.exports = function Cart(initItems){
-    this.items = initItems;
-    this.totalQty = 0;
-    this.toalPrice = 0;
+module.exports = function Cart(oldCart){
+    this.items = oldCart.items;
+    this.totalQty = oldCart.totalQty;
+    this.toalPrice = oldCart.totalPrice;
     
     this.addnewItems = function(item, id){
         //Will not a product if it already exists in the cart if it exists, then just update the quantity.
-        
-        var storedItem = this.items[id];
-        
-    }
+                var storedItem = this.items[id];
+
+                if(!storedItem){
+                    storedItem = items[id] = {item :item, quantity: 0, price: 0};
+                }
+                storedItem.quantity++;
+                storedItem.price = storedItem.item.price * storedItem.quantity;
+                this.totalQty++;
+                this.totalPrice += storedItem.price;
+            }
+    
+     this.generateArray = function(){
+            var arr = [];
+            for(var id in this.items){
+                arr.push(this.items[id]);
+            }
+            
+            return arr;
+        };
     
 };
