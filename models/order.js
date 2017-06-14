8e493fe98697c621f20536aaf947997c7e6b60c6
kvin1087 <kaustubh87@gmail.com>
2017-06-14 10:52:27 -0500

    Added model for orders to store in the database

---
+++ models/order.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var orderSchema = new Schema({
+   user: {
+       type: Schema.Types.ObjectId,
+       ref: 'User'
+   },
+    cart: {
+        type: Object,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    }
+});
+
+
+module.exports = mongoose.model('Order', orderSchema);
