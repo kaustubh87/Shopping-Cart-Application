b18c622c08bc1c6b222a444fe731ff542c0276a9
kvin1087 <kaustubh87@gmail.com>
2017-06-16 16:44:16 -0500

    routes/index.js
    added two functions

--- config/passport.js
+++ config/passport.js
@@ -50,6 +50,40 @@ passport.use('local.signup', new LocalStrategy({
            return done(null, newUser);
        });
    });
+       
+}));
 
+/*Sign In Strategy with Validation */
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function(req,email,password, done){
     
+    req.checkBody('email', 'Invalid Email').notEmpty().isEmail();
+    req.checkBody('password','Invalid Password').notEmpty();
+    var errors = req.validationErrors();
+    if(errors){
+        var messages = [];
+        errors.forEach(function(error){
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error' , messages));
+    }
+    
+    User.findOne({'email': email}, function(err, user){
+      if(err){
+          return done(err);
+      } 
+       if(!user){
+           return done(null, false, {message: 'No User found.'});
+       }
+        if(!user.validPassword(password)) {
+            return done(null, false, {message: 'Wrong Password'});
+        }
+      return done(null, user);
+   });
+    
+
 }));
